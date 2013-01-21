7e9a37194071e7eba55d8843424a6ee74565550d
Greg Reinbach <greg@reinbach.com>
2013-01-20 20:12:40 -0500

    removed django framework for website

--- website/finance/core/views.py
+++
@@ -1,4 +0,0 @@
-from shortcuts import request_to_response
-
-def home(request):
-    return request_to_response(request, "login.html", {})
--- website/finance/settings/__init__.py
+++
@@ -1,8 +0,0 @@
-try:
-    from .currentenv import *
-except ImportError:
-    print ('Error importing settings/currentenv.py. Did you forget to symlink your '
-           'local settings?')
-
-    import traceback
-    print traceback.print_exc()
