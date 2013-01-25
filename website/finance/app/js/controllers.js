6d27618557c4cdbf4b4ac79476bbaf7a5a1f52a7
Greg Reinbach <greg@reinbach.com>
2013-01-25 05:44:04 -0500

    cleaned up login controller removed test code redirect to /accounts on successful login

--- website/finance/app/js/controllers.js
+++ website/finance/app/js/controllers.js
@@ -3,18 +3,17 @@
 /* Controllers */
 
 
-function FinanceCtrlLogin($scope, $http) {
+function FinanceCtrlLogin($scope, $http, $location) {
     $scope.response = "";
     $scope.login = function() {
         $scope.response = "";
         $http.post('http://localhost:5000/login', $scope.user).
             success(function(data){
                 // redirect to accounts page
+                $location.path("/accounts");
             }).
             error(function(data){
                 $scope.response = data.message;
-                //test
-                console.log($scope.response);
             });
     }
 }
