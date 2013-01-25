2331121dc232b2a00143ad5b1be5124ff9e64d6c
Greg Reinbach <greg@reinbach.com>
2013-01-25 05:43:25 -0500

    Added an interceptor to better handle any 401 response

--- website/finance/app/js/app.js
+++ website/finance/app/js/app.js
@@ -7,4 +7,26 @@ angular.module('financeApp', ['financeApp.filters', 'financeApp.services', 'fina
         $routeProvider.when('/login', {templateUrl: 'partials/login.html', controller: FinanceCtrlLogin});
         $routeProvider.when('/accounts', {templateUrl: 'partials/accounts.html', controller: FinanceCtrlAccounts});
         $routeProvider.otherwise({redirectTo: '/login'});
-    }]);
+    }]).
+    config(function($httpProvider) {
+        var interceptor = ['$location', '$q', function($location, $q) {
+            function success(response) {
+                return response;
+            };
+
+            function error(response) {
+                if (response.status == 401) {
+                    var deferred = $q.defer();
+                    $location.path("/login");
+                    return deferred.promise;
+                };
+                return $q.reject(response);
+            };
+
+            return function(promise) {
+                return promise.then(success, error);
+            };
+        }];
+
+        $httpProvider.responseInterceptors.push(interceptor);
+    });
