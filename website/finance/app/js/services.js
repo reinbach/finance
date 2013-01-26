1858f537dec4f8a133d59a679c19d7d31a8017a6
Greg Reinbach <greg@reinbach.com>
2013-01-26 08:50:09 -0500

    removed test code

--- website/finance/app/js/services.js
+++ website/finance/app/js/services.js
@@ -6,15 +6,4 @@
 // Demonstrate how to register services
 // In this case it is a simple value service.
 angular.module('financeApp.services', []).
-    value('version', '0.1').
-    factory('notify', ['$window', function(win) {
-        var msgs = [];
-        return function(msg) {
-            msgs.push(msg);
-            if (msgs.length == 3) {
-                win.alert(msgs.join("\n"));
-                msgs = [];
-            }
-        };
-    }]);
-
+    value('version', '0.1');
