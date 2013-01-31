2894ffe3e29bcc6435a9bc74c47079117e9c9737
Greg Reinbach <greg@reinbach.com>
2013-01-30 20:05:56 -0500

    added secure directive that is set by route params, elements can be hidden is route passes in secure

--- website/finance/app/js/app.js
+++ website/finance/app/js/app.js
@@ -5,7 +5,7 @@
 angular.module('financeApp', ['financeApp.filters', 'financeApp.services', 'financeApp.directives']).
     constant('api_url', 'http://localhost\\:5000').
     config(['$routeProvider', function($routeProvider) {
-        $routeProvider.when('/login', {templateUrl: 'partials/login.html', controller: FinanceCtrlLogin});
+        $routeProvider.when('/login', {templateUrl: 'partials/login.html', controller: FinanceCtrlLogin, secure: true});
         $routeProvider.when('/accounts', {templateUrl: 'partials/accounts.html', controller: FinanceCtrlAccounts});
         $routeProvider.otherwise({redirectTo: '/login'});
     }]).
--- website/finance/app/js/controllers.js
+++ website/finance/app/js/controllers.js
@@ -2,13 +2,22 @@
 
 /* Controllers */
 
+function financeCtrl($scope, $rootScope, $location) {
+    $rootScope.$on('$routeChangeSuccess', function(event, routeData) {
+        if (routeData.$route) {
+            $scope.secure = routeData.$route.secure;
+        }
+    });
+}
 
-function FinanceCtrlLogin(scope, http, location, api_url) {
+function FinanceCtrlLogin(scope, http, location, tokenHandler, api_url) {
     scope.response = "";
     scope.login = function() {
         scope.response = "";
         http.post(api_url.replace('\\', '') + '/login', scope.user).
             success(function(data){
+                // set token
+                tokenHandler.set(data.auth_token);
                 // redirect to accounts page
                 location.path("/accounts");
             }).
@@ -17,7 +26,7 @@ function FinanceCtrlLogin(scope, http, location, api_url) {
             });
     }
 }
-FinanceCtrlLogin.$inject = ['$scope', '$http', '$location', 'api_url'];
+FinanceCtrlLogin.$inject = ['$scope', '$http', '$location', 'tokenHandler', 'api_url'];
 
 
 function FinanceCtrlAccounts(scope, AccountService) {
