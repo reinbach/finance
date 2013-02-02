8d8f3c189ff263db2ac86a845e769c79d135a625
Greg Reinbach <greg@reinbach.com>
2013-02-02 13:26:58 -0500

    added dropdown toggle directive

--- website/finance/app/js/directives.js
+++ website/finance/app/js/directives.js
@@ -13,4 +13,52 @@ angular.module('financeApp.directives', []).
         return function(scope, element) {
             element[0].focus();
         };
-    });
+    }).
+    directive('dropdownToggle', ['$document', '$location', '$window', function ($document, $location, $window) {
+        var openElement = null, close;
+        return {
+            restrict: 'CA',
+            link: function(scope, element, attrs) {
+                scope.$watch(
+                    function dropdownTogglePathWatch() { return $location.path(); },
+                    function dropdownTogglePathWatchAction() {
+                        if (close) { close(); }
+                    }
+                );
+
+                element.parent().bind('click', function(event) {
+                    if (close) { close(); }
+                });
+
+                element.bind('click', function(event) {
+                    event.preventDefault();
+                    event.stopPropagation();
+
+                    var iWasOpen = false;
+
+                    if (openElement) {
+                        iWasOpen = openElement === element;
+                        close();
+                    }
+
+                    if (!iWasOpen){
+                        element.parent().addClass('open');
+                        openElement = element;
+
+                        close = function (event) {
+                            if (event) {
+                                event.preventDefault();
+                                event.stopPropagation();
+                            }
+                            $document.unbind('click', close);
+                            element.parent().removeClass('open');
+                            close = null;
+                            openElement = null;
+                        };
+
+                        $document.bind('click', close);
+                    }
+                });
+            }
+        };
+    }]);
