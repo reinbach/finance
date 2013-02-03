7ba761db05a2b5c88a4b73652c19fb9a36eaab7d
Greg Reinbach <greg@reinbach.com>
2013-02-03 10:02:47 -0500

    added the relevant test files for account type

---
+++ api/tests/account_type_model_tests.py
@@ -0,0 +1,51 @@
+import json
+import unittest
+
+from sqlalchemy.exc import IntegrityError
+
+from finance.models import AccountType, db_session
+
+class AccountTypeModelTestCase(unittest.TestCase):
+
+    def tearDown(self):
+        db_session.remove()
+
+    def test_account_type_repr(self):
+        at = AccountType('Expenses')
+        self.assertTrue(at)
+
+    def test_account_type_add(self):
+        """Test adding an account type normally"""
+        at = AccountType('Random')
+        self.assertEqual(at.name, 'Random')
+
+        db_session.add(at)
+        db_session.commit()
+
+        at2 = AccountType.query.filter(AccountType.name == at.name).first()
+        self.assertEqual(at2.name, at.name)
+        self.assertTrue(at2.account_type_id)
+
+    def test_account_type_name_unique(self):
+        """Test that account type name uniqueness is maintained"""
+        at = AccountType('Interest Income')
+        db_session.add(at)
+        db_session.commit()
+
+        with self.assertRaisesRegexp(
+            IntegrityError,
+            'violates unique constraint "account_types_name_key"'
+        ):
+            at2 = AccountType("Interest Income")
+            db_session.add(at2)
+            db_session.commit()
+
+    def test_account_type_jsonify(self):
+        """Test the jsonify method"""
+        at = AccountType('Income')
+        self.assertEqual(dict, type(at.jsonify()))
+        self.assertTrue(json.dumps(at.jsonify()))
+
+test_cases = [
+    AccountTypeModelTestCase,
+]
---
+++ api/tests/account_type_view_tests.py
@@ -0,0 +1,165 @@
+import json
+
+from finance.models import AccountType, db_session
+from view_tests import BaseViewTestCase
+
+class AccountTypeViewTestCase(BaseViewTestCase):
+
+    def setUp(self):
+        super(AccountTypeViewTestCase, self).setUp()
+        self.account_type = AccountType('Expense')
+        db_session.add(self.account_type)
+        db_session.commit()
+
+    def tearDown(self):
+        db_session.delete(self.account_type)
+        super(AccountTypeViewTestCase, self).tearDown()
+
+    def test_view_auth_required(self):
+        """Test that authentication is required"""
+        rv = self.app.get("/account/types")
+        self.assertEqual(401, rv.status_code)
+
+    def test_view_all(self):
+        """Test viewing all account types"""
+        rv = self.open_with_auth(
+            "/account/types",
+            'GET',
+            self.username,
+            self.password
+        )
+        self.assertEqual(200, rv.status_code)
+        data = json.loads(rv.data)
+        acct_type = self.account_type.jsonify()
+        self.assertIn(acct_type, data['account_types'])
+
+    def test_view_account(self):
+        """Test viewing a single account type"""
+        rv = self.open_with_auth(
+            "/account/types/%s" % self.account_type.account_type_id,
+            "GET",
+            self.username,
+            self.password
+        )
+        self.assertEqual(200, rv.status_code)
+        self.assertEqual(self.account_type.jsonify(), json.loads(rv.data))
+
+    def test_view_account_404(self):
+        """Test viewing a non-existant account type"""
+        rv = self.open_with_auth(
+            "/account/types/999",
+            "GET",
+            self.username,
+            self.password
+        )
+        self.assertEqual(404, rv.status_code)
+        self.assertIn("404 Not Found", rv.data)
+
+    def test_view_add(self):
+        """Test adding an account type"""
+        name = 'Supplies'
+        rv = self.open_with_auth(
+            "/account/types",
+            "POST",
+            self.username,
+            self.password,
+            data=dict(
+                name=name,
+            )
+        )
+        self.assertEqual(200, rv.status_code)
+        self.assertIn('Success', rv.data)
+
+        acct_type = json.loads(rv.data)
+        rv = self.open_with_auth(
+            "/account/types/%s" % acct_type.get('account_type_id'),
+            "GET",
+            self.username,
+            self.password,
+        )
+
+        self.assertEqual(200, rv.status_code)
+        acct_type_get = json.loads(rv.data)
+        self.assertEqual(name, acct_type_get.get('name'))
+        self.assertEqual(acct_type.get('account_type_id'), acct_type_get.get('account_type_id'))
+
+    def test_view_add_fail(self):
+        """Test adding an invalid account type"""
+        name = 'Sup'
+        rv = self.open_with_auth(
+            "/account/types",
+            "POST",
+            self.username,
+            self.password,
+            data=dict(
+                name=name,
+            )
+        )
+        self.assertEqual(400, rv.status_code)
+        self.assertIn('errors', rv.data)
+
+    def test_view_delete(self):
+        """Test deleting account"""
+        account_type1 = AccountType(
+            'Entertainment',
+        )
+        db_session.add(account_type1)
+        db_session.commit()
+        rv = self.open_with_auth(
+            "/account/types/%s" % account_type1.account_type_id,
+            "DELETE",
+            self.username,
+            self.password
+        )
+        self.assertEqual(200, rv.status_code)
+
+        # attempt to get the account
+        rv = self.open_with_auth(
+            "/account/types/%s" % account_type1.account_type_id,
+            "GET",
+            self.username,
+            self.password
+        )
+        self.assertEqual(404, rv.status_code)
+
+    def test_view_delete_fail(self):
+        """Test deleting a non existant account type"""
+        rv = self.open_with_auth(
+            "/account/types/999",
+            "DELETE",
+            self.username,
+            self.password
+        )
+        self.assertEqual(404, rv.status_code)
+
+    def test_view_update(self):
+        """Test updating an account type"""
+        name = 'Entertainment'
+        account_type_id = self.account_type.account_type_id
+        rv = self.open_with_auth(
+            "/account/types/%s" % account_type_id,
+            "PUT",
+            self.username,
+            self.password,
+            data=dict(
+                name=name,
+            )
+        )
+        self.assertEqual(200, rv.status_code)
+        self.assertIn('Success', rv.data)
+
+        rv = self.open_with_auth(
+            "/account/types/%s" % account_type_id,
+            "GET",
+            self.username,
+            self.password,
+        )
+
+        self.assertEqual(200, rv.status_code)
+        acct_type_get = json.loads(rv.data)
+        self.assertEqual(name, acct_type_get.get('name'))
+
+
+test_cases = [
+    AccountTypeViewTestCase
+]
