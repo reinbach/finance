dda64ab37fc0c647382960b82b5aa1102c802857
Greg Reinbach <greg@reinbach.com>
2013-08-02 08:40:27 -0400

    bumped Flask version, tweaked app details in setup

--- api/setup.py
+++ api/setup.py
@@ -2,7 +2,7 @@
 from setuptools import setup
 
 requires = [
-    'Flask==0.9',
+    'Flask==0.10.1',
     'WTForms==1.0.2',
     'SQLAlchemy==0.8.0b2',
     'psycopg2==2.4.5',
@@ -11,9 +11,9 @@ requires = [
 ]
 
 setup(
-    name='Finance',
-    version='1.0',
-    description='API tracking accounts',
+    name='finance',
+    version='0.1',
+    description='Finance API for tracking accounts',
     author='Greg Reinbach',
     author_email='greg@reinbach.com',
     url='https://github.com/reinbach/finance',
