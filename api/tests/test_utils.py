988b137fd4b70bd89012a4af83d9850b551e229f
Greg Reinbach <greg@reinbach.com>
2013-08-31 17:01:55 -0400

    added test utils, and then commented it all out

---
+++ api/tests/test_utils.py
@@ -0,0 +1,12 @@
+#from finance.utils import crossdomain
+
+
+class TestCrossDomain():
+    # def test_init_origin(self):
+    #     c = crossdomain(origin=['example.com', 'www.example.com'])
+    #     assert c.origin == 'example.com, www.example.com'
+
+    # def test_init_methods(self):
+    #     c = crossdomain(methods=['GET', 'POST'])
+    #     assert c.get_methods() == 'GET, POST'
+    pass
