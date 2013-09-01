24881bcef876b95c18e693623cec3895524dba1a
Greg Reinbach <greg@reinbach.com>
2013-09-01 14:12:14 -0400

    cleaned up test setup, now better coverage reporting

--- api/tests/conftest.py
+++ api/tests/conftest.py
@@ -1,9 +1,3 @@
 import config
 
 config.TESTING = True
-
-from tests.fixtures import (filename, setup_account_types, setup_accounts,
-                            setup_transactions)
-
-__all__ = ['filename', 'setup_account_types', 'setup_accounts',
-           'setup_transactions']
