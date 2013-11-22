80c05f002155d446c26421398b95319811c549dd
Greg Reinbach <greg@reinbach.com>
2013-11-22 05:53:59 -0500

    updated auth test to user correct object

--- api/tests/models/test_auth.py
+++ api/tests/models/test_auth.py
@@ -1,6 +1,6 @@
 import pytest
 
-from finance.models.user import User, db
+from finance.models.auth import AuthUser, db
 from sqlalchemy.exc import IntegrityError
 
 
@@ -8,30 +8,30 @@ class TestUserModel():
 
     def test_user_repr(self):
         """Ensure __repr__ function works"""
-        u = User('test', 'secret')
+        u = AuthUser('test', 'secret')
         assert repr(u) == "<User: test>"
 
     def test_user_add(self):
         """Test adding a user normally"""
-        u = User('test', 'secret')
+        u = AuthUser('test', 'secret')
         assert u.username == "test"
 
         db.session.add(u)
         db.session.commit()
 
-        u2 = User.query.filter(User.username == u.username).first()
+        u2 = AuthUser.query.filter(AuthUser.username == u.username).first()
         assert u2.username == u.username
         assert bool(u2.user_id) is True
         assert u2.password_hash == u.password_hash
 
     def test_user_name_unique(self):
         """Test user name uniqueness is maintained"""
-        u = User('test_unique', 'secret')
+        u = AuthUser('test_unique', 'secret')
         db.session.add(u)
         db.session.commit()
 
         with pytest.raises(IntegrityError):
-            u2 = User('test_unique', 'secret')
+            u2 = AuthUser('test_unique', 'secret')
             db.session.add(u2)
             db.session.commit()
         db.session.rollback()
@@ -39,7 +39,7 @@ class TestUserModel():
     def test_user_password(self):
         """Test user password is hashed"""
         password = 'secret'
-        u = User('test2', password)
+        u = AuthUser('test2', password)
 
         with pytest.raises(AttributeError):
             u.password
